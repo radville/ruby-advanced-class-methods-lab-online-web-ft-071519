97b83b3d8d6f9ad78863ea0e6222784696f6f019
Laura Radville <laura.radville@gmail.com>
2019-07-24 16:15:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -13,6 +15,7 @@ class Song
   def self.create
     @@all << self.new
     return self
+    binding.pry
   end
 
 end
