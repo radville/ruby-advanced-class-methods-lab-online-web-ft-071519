fafa3568d77212273afb9a6efea8dc9f0e71670a
Laura Radville <laura.radville@gmail.com>
2019-07-24 16:13:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,10 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize
+    
+  end
+
   def self.all
     @@all
   end
@@ -9,8 +13,5 @@ class Song
   def save
     self.class.all << self
   end
-  
-  def self.create(song)
-    self.class.all << song
-  end
+
 end
