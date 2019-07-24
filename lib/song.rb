cb7083f4c64a0e760f6e34ceb3195ca921dffba0
Laura Radville <laura.radville@gmail.com>
2019-07-24 17:35:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,13 +20,15 @@ class Song
   
   def self.new_by_name(song_name)
     song = self.new
-    song.@name = song_name
+    song.name = song_name
     song
   end
   
   def self.create_by_name(song_name)
+    binding.pry
     song = self.new_by_name(song_name)
     @@all << song
+    
   end
 
 end
