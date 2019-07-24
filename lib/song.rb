b1cbb66ff30e33196795ef1928459937901407dd
Laura Radville <laura.radville@gmail.com>
2019-07-24 19:06:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,7 +60,7 @@ class Song
   end
   
   def self.create_from_filename(filename)
-    self.new_from_filename(filename).create_by_name(filename)
+    self.new_from_filename.create_by_name(filename)
     # binding.pry
   end
 
